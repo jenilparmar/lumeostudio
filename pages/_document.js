535ecf8f42dec424fab2a43398cdac0b2a1f3289
jenilparmar <jerryhash231@gmail.com>
2025-02-09 16:38:45 +0530

    sdgfh

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-import { Analytics } from "@vercel/analytics/react"
 export default function Document() {
   return (
     <Html lang="en">
@@ -8,7 +7,7 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
-        <Analytics />
+    
       </body>
     </Html>
   );
